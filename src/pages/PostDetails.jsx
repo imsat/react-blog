2ca49bb821f91823ce90bfba5b2c16fa4705e9b5
Anindow <anindowsaikat@gmail.com>
2023-03-02 20:06:51 +0600

    nothing

--- src/pages/PostDetails.jsx
+++ src/pages/PostDetails.jsx
@@ -18,27 +18,29 @@ function PostDetails() {
   return (
     <>
       <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 className="h2">Post Details</h1>
+        <h1 className="h2 m-auto">Post Details</h1>
       </div>
       <div className="row">
-        <div className="card">
-          <div className="card-body">
-            <div className="row">
-              <dt className="col-sm-2">ID</dt>
-              <dd className="col-sm-10">{post.id}</dd>
-            </div>
-            <div className="row">
-              <dt className="col-sm-2">Title</dt>
-              <dd className="col-sm-10">{post.title}</dd>
-            </div>
-            <div className="row">
-              <dt className="col-sm-2">Body</dt>
-              <dd className="col-sm-10">{post.body}</dd>
+        <div className="col-4 m-auto">
+          <div className="card bg-info bg-opacity-25">
+            <div className="card-body">
+              <div className="row">
+                <dt className="col-sm-2">ID</dt>
+                <dd className="col-sm-10">{post.id}</dd>
+              </div>
+              <div className="row">
+                <dt className="col-sm-2">Title</dt>
+                <dd className="col-sm-10">{post.title}</dd>
+              </div>
+              <div className="row">
+                <dt className="col-sm-2">Body</dt>
+                <dd className="col-sm-10">{post.body}</dd>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </>
-  );
+  )
 }
 export default PostDetails;
